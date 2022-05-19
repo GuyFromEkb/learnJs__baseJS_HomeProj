1605fa1504a8dc2c140f81042c10427914757b9c
Sergey <graygmai1@gmail.com>
2022-05-19 13:46:50 +0500

    refactoring

--- task/js/script.js
+++ task/js/script.js
@@ -13,35 +13,32 @@
 5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
+
+const deleteAds = document.querySelector('.promo__adv'),
+    afishaInfo = document.querySelector('.promo__bg'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против...",
+            "Скотт Пилигрим против...",
+            "Скотт Пилигрим против...",
+        ]
+    };
+
 //1
-const deleteAds = document.querySelector('.promo__adv');
 deleteAds.remove();
 //2
-const afishaInfo = document.querySelector('.promo__bg');
 afishaInfo.children[0].textContent = "драма";
 //3
-afishaInfo.style.background = "url('./img/bg.jpg') center(center / cover) no - repeat";
-
-
-
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-    ]
-};
+afishaInfo.style.backgroundImage = 'url(img/bg.jpg)';
 
 //4,5
+movieList.innerHTML = "";
 movieDB.movies.sort();
-
-const movieList = document.querySelectorAll('.promo__interactive-item');
-
-for (let i = 0; i < movieList.length; i++) {
-
-    movieList[i].textContent = `${i+1}. ${movieDB.movies[i]}`;
-
-}
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `<li class = "promo__interactive-item">${i+1}. ${film} <div class = "delete"> </div> </li>`;
+});
