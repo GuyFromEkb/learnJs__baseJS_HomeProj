42a34394abbd63de1a94f15f74078a733815238b
Sergey <graygmai1@gmail.com>
2022-05-30 11:24:18 +0500

    Делегирования событтий песочница

--- testZone/js/sandbox.js
+++ testZone/js/sandbox.js
@@ -1,12 +1,24 @@
 "use strict";
+const btn = document.querySelectorAll('button');
+const wrap = document.querySelector('.bg');
 
-function pow(x, n) {
-    if (n <= 1) return x;
-    else {
-        return x * pow(x, n - 1);
+const btnFromJS = document.createElement("button");
+btnFromJS.classList.add('blue');
+// document.querySelector('.bg').append(btnFromJS);
+
+wrap.addEventListener('click', (e) => {
+
+    // console.dir(e.target);
+    if (e.target && e.target.tagName == "BUTTON") {
+        console.log("block if");
+        wrap.append(document.createElement("button"));
     }
 
-}
+});
 
-
-console.log(pow(6, 7));
+// btn.forEach(element => {
+//     element.addEventListener('click', function(e) {
+//         e.target.classList.toggle('red');
+//         // console.dir(e.target);
+//     });
+// });
