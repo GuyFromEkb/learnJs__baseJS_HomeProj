61f1f65bae520aadd154d3459a5cb1ee86d45198
Sergey <graygmai1@gmail.com>
2022-06-03 05:57:38 +0500

    Табы на сайте

--- foodSite/js/main.js
+++ foodSite/js/main.js
@@ -1,72 +1,46 @@
-// const tabImg = document.querySelectorAll('.tabcontent');
-// const tabBtn = document.querySelectorAll('.tabheader__item');
-
-
-// function hideTabContent() {
-//     tabImg.forEach(item => {
-//         item.classList.add('hide', 'fade');
-//         item.classList.remove('show', 'fade');
-//     });
-//     tabBtn.forEach(item => {
-//         item.classList.remove('tabheader__item_active')
-//     });
-// }
-
-// function showTabContect(i = 0) {
-//     tabImg[i].classList.add('show', 'fade');
-//     tabImg[i].classList.remove('hide');
-
-//     tabBtn[i].classList.add('tabheader__item_active');
-// }
-// hideTabContent();
-// showTabContect();
-
-// tabBtn.forEach((item, i) => {
-//     item.addEventListener('click', () => {
-//         hideTabContent();
-//         showTabContect(i);
-//     });
-// });
-
-
+"use strict";
 window.addEventListener('DOMContentLoaded', function() {
 
-    // Tabs
+    //Tabs
+    const tabsImg = document.querySelectorAll('.tabcontent'),
+        tabsName = document.querySelectorAll('.tabheader__item'),
+        tabsSelector = document.querySelector('.tabheader__items');
 
-    let tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
-
-    function hideTabContent() {
-
-        tabsContent.forEach(item => {
+    function hideTabs() {
+        tabsImg.forEach(item => {
+            item.classList.remove('show');
             item.classList.add('hide');
-            item.classList.remove('show', 'fade');
         });
-
-        tabs.forEach(item => {
+        tabsName.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     }
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
+    function showTab(tabNumber = 0) {
+        tabsImg[tabNumber].classList.add('show', 'showTab');
+        tabsName[tabNumber].classList.add('tabheader__item_active');
     }
 
-    hideTabContent();
-    showTabContent();
+    hideTabs();
+    showTab();
 
-    tabsParent.addEventListener('click', function(event) {
-        const target = event.target;
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
+    tabsSelector.addEventListener('click', function(e) {
+        let target = e.target;
+        if (target.classList.contains('tabheader__item')) {
+            tabsName.forEach((item, index) => {
                 if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
+                    hideTabs();
+                    showTab(index);
                 }
             });
         }
+
     });
+
+
+
+
+
+
+
 });
