d5cf226bfba4e8776cb554f5f9a254c7838a2963
Sergey_nasty <zaebalacs@gmail.com>
2022-06-13 05:06:44 +0500

    Знакомство с классом  Promise

--- testZone/js/Task.js
+++ testZone/js/Task.js
@@ -1,6 +1,38 @@
-const j = 'ab';
-const s = 'aabbccd';
+console.log("Запуск приложения")
 
-const vs = (s.split('').filter(char => j.includes(char)).join(''));
+const prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Загрузка данных");
+        resolve();
 
-console.log(vs.length)
+    }, 2000);
+});
+
+prom.then(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Данные загружены");
+            const obj = {
+                name: "Tv-box",
+                price: 3000
+            };
+            resolve(obj);
+        }, 2000);
+    });
+}).
+then(obj => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Данные изменены");
+            obj['order'] = true;
+            console.log(obj);
+            resolve(obj);
+        }, 2000);
+    });
+}).then(obj => {
+    setTimeout(() => {
+        console.log("Заказ отправлен");
+        obj['status'] = "delivery";
+        console.log(obj);
+    }, 2000);
+});
